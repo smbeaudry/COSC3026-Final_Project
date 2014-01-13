37fe15d33d2ceb1a1afef2289520ad05f2366fcf
Luc <Luc@lucslaptop>
2014-01-13 12:45:36 -0500

    Wrote basic room class, will add comment later.

--- GameDesign/src/gamedesign/Room.java
+++ GameDesign/src/gamedesign/Room.java
@@ -11,5 +11,34 @@ package gamedesign;
  * @author Luc Breault
  */
 public class Room {
+    protected boolean visited;
+    protected Room[] neighbours;
+    protected boolean[] doors;
+    public static final int NORTH = 0, WEST = 1, SOUTH = 2, EAST = 3;
     
+    public Room(){
+        visited = false;
+        neighbours = new Room[4];
+        doors = new boolean[4];
+    }
+    
+    public void setVisited(boolean isVisited){
+        visited = isVisited;
+    }
+    
+    public boolean isVisited(){
+        return visited;
+    }
+    
+    public void setNeighbour(int index, Room neighbour){
+        neighbours[index] = neighbour;
+    }
+    
+    public Room getNeighbour(int index){
+        return neighbours[index];
+    }
+    
+    public boolean hasDoor(int index){
+        return doors[index];
+    }
 }

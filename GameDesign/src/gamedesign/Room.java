2eb2895df96b1b49672c96b31670f794f3b9114b
dpells <da.pellegrini02@gmail.com>
2014-01-14 16:24:08 -0500

    Added constructor with params and a roomType

--- GameDesign/src/gamedesign/Room.java
+++ GameDesign/src/gamedesign/Room.java
@@ -14,12 +14,39 @@ public class Room {
     protected boolean visited;
     protected Room[] neighbours;
     protected boolean[] doors;
+    protected String roomType; //used for room graphic
     public static final int NORTH = 0, WEST = 1, SOUTH = 2, EAST = 3;
     
     public Room(){
         visited = false;
         neighbours = new Room[4];
         doors = new boolean[4];
+        roomType = null;
+    }
+    
+    public Room(Room[] neighbours, boolean[] doors, String roomType)
+    {
+        visited = false;
+        //the "this" keyword references the protected data fields
+        this.neighbours = new Room[neighbours.length];
+        //copies neighbours array param
+        for(int i = 0; i <= neighbours.length; i++)
+            this.neighbours[i] = neighbours[i];
+        //copies doors array param
+        this.doors = new boolean[doors.length];
+        for(int i = 0; i <= doors.length; i++)
+            this.doors[i] = doors[i];
+        this.roomType = roomType;
+    }
+    
+    public void setRoomType(String roomType)
+    {
+        this.roomType = roomType;
+    }
+    
+    public String getRoomType()
+    {
+        return roomType;
     }
     
     public void setVisited(boolean isVisited){

29fe4080927ef9bdc684f7efe865523e9c061af6
Luc <Luc@142.51.185.103>
2014-01-16 15:07:42 -0500

    Added code to the shuffleMap method

--- GameDesign/src/gamedesign/GameDesign.java
+++ GameDesign/src/gamedesign/GameDesign.java
@@ -6,6 +6,7 @@
 
 package gamedesign;
 
+import java.util.Random;
 /**
  *
  * @author The team
@@ -14,17 +15,31 @@ package gamedesign;
 public class GameDesign {
 
     public static Room[][] map;
+    public static Random randNumber;
+    public static long seed;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        randNumber = new Random();
         
     }
     
-    public static void shuffleMap(){
-        int maxRooms = (int)((Math.random() * 60) + 1);
+    /**
+     * This method creates a randomly generated map based on the seed given.
+     * The seed is normally generated using the following code:
+     * java.util.Date test = new java.util.Date();
+     * long seed = test.getTime());
+     * 
+     * @author Luc Breault
+     * @param seed The seed which will determine the map to be used
+     * 
+     */
+    public static void shuffleMap(long seed){
+        //seed to allow replay of same map and multiplayer game synchronizing
+        randNumber.setSeed(seed);
+        int maxRooms = randNumber.nextInt(60) + 1;
         //Map maximum size
         map = new Room[10][10];
         int centerX = map.length / 2;
@@ -33,8 +48,46 @@ public class GameDesign {
         Room room = new Room();
         room.setVisited(true);
         //(length [10] / 2) == 5 therefore position 6,6
-        map[centerX][centerY] = new Room();
+        map[centerX][centerY] = room;
         
+        int x = centerX, y = centerY;
+                
+        room.setNeighbour(Room.NORTH, new Room(map[x][y], Room.SOUTH));
+        map[x][y + 1] = room.getNeighbour(Room.NORTH);
+        
+        room.setNeighbour(Room.WEST, new Room(map[x][y], Room.EAST));
+        map[x - 1][y] = room.getNeighbour(Room.WEST);
+        
+        room.setNeighbour(Room.SOUTH, new Room(map[x][y], Room.NORTH));
+        map[x][y - 1] = room.getNeighbour(Room.SOUTH);
+        
+        room.setNeighbour(Room.EAST, new Room(map[x][y], Room.WEST));
+        map[x + 1][y] = room.getNeighbour(Room.EAST);
+        
+        int dir1, dir2;
+        for(int i = 0; i < maxRooms; i++){
+            int rand = randNumber.nextInt(4);
+            if(map[x][y].hasEmptyNeighbour() > -1){
+                while(map[x][y].getNeighbour(rand) != null){
+                    randNumber.nextInt(4);
+                }
+                if(rand % 4 == Room.NORTH){
+                    dir1 = Room.NORTH;
+                    dir2 = Room.SOUTH;
+                } else if (rand % 4 == Room.SOUTH){
+                    dir1 = Room.SOUTH;
+                    dir2 = Room.NORTH;
+                } else if (rand % 4 == Room.EAST){
+                    dir1 = Room.EAST;
+                    dir2 = Room.WEST;
+                } else {
+                    dir1 = Room.WEST;
+                    dir2 = Room.EAST;
+                }
+                map[x][y].setNeighbour(dir1, new Room(map[x][y], dir2));
+                map[x][y] = map[x][y].getNeighbour(dir1);
+            }
+        }
         
     }
     
